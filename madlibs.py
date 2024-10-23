a412f6dd398840091a4c9f80e1805373556fcea0
Elizabeth <elizabethg2796@tuckahoeschools.org>
2024-10-23 12:30:43 +0000

    intial commit

---
+++ madlibs.py
@@ -0,0 +1,11 @@
+verb1 = input("Type a verb: ")
+relationship1 = input("Type a relation to another person here (ex. = friend, coworker, husband, cousin): ")
+noun1 = input("Type a noun: ")
+transportation1 = input("Type a kind of transportation : ")
+number1 = input("Type a number: ")
+
+print(f"""On an early spring morning, Tracy {verb1} to her job in hopes of impressing Bianca. 
+Tracy jumped over {noun1} and rode on the roof of a {transportation1} all in an attempt to arrive just (number1) minutes late. 
+She walked 
+
+""")
