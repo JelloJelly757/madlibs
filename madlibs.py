8f653bd23351b369a613e49d94818b2003a02d40
Elizabeth <elizabethg2796@tuckahoeschools.org>
2024-10-24 01:18:22 +0000

    finished up story

--- madlibs.py
+++ madlibs.py
@@ -1,11 +1,37 @@
-verb1 = input("Type a verb: ")
-relationship1 = input("Type a relation to another person here (ex. = friend, coworker, husband, cousin): ")
-noun1 = input("Type a noun: ")
-transportation1 = input("Type a kind of transportation : ")
-number1 = input("Type a number: ")
+adjective3 = input("Type an adjective: ")
+noise1 = input("Type a noise: ")
+dessert1 = input("Type a dessert: ")
+food1 = input("Type a food: ")
+verb1 = input("Type a past tense verb (verb should end with -ed): ")
+food2 = input("Type a food: ")
+drink1 = input("Type a drink: ")
+adjective1 = input("Type an adjective: ")
+car1 = input("Type a type of car: ")
+bodypart1 = input("Type a bodypart: ")
+store1 = input("Type a store: ")
+adjective2 = input("Type an adjective: ")
+adverb1 = input("Type an adverb: ")
+food3 = input("Type an ingredient: ")
+food4 = input("Type a ingredient: ")
+food5 = input("Type a ingredient: ")
+food6 = input("Type a ingredient: ")
+color1 = input("Type a color: ")
 
-print(f"""On an early spring morning, Tracy {verb1} to her job in hopes of impressing Bianca. 
-Tracy jumped over {noun1} and rode on the roof of a {transportation1} all in an attempt to arrive just (number1) minutes late. 
-She walked 
 
+print(f"""I woke up on Saturday morning with a {adjective3} hampering. 
+I jumped to my feet as I heard my stomach make a strange sound. A {noise1}. 
+This wasn't a normal stomach noise. Only one thing could cure my hunger:
+my mother's famous {dessert1}. I {verb1} to the kitchen, trying to rememeber how it was made. 
+I couldn't ask my mother since we were currently arguing over my lack of indpendence. 
+Maybe she had a point. I opened my fridge and glanced inside. 
+The food options were scarce: {food1}, {food2}, and {drink1}. 
+All of which were stocked by my mother. Maybe she really did have a solid point. 
+I grabbed my keys and ran outside. I saw my {adjective1} car, a {car1}. 
+I hopped in and pressed my {bodypart1} down on the brake. I arrived at the store, {store1}. 
+It was a {adjective2} store, with foods lining every aisle. 
+I {adverb1} grabbed the ingredients I needed: {food3}, {food4}, {food5}, and {food6}. 
+I raced back to my house in hopes of conquering my hunger. 
+Long story short, I have no clue how to bake (maybe my mother really has a point). 
+But at the very least the once cold morning got a little hotter as I stood outside and watched my house alight in a {color1} ember. 
+Well at least I wouldn't have to pay my mortgage next month. I should really call my mom. 
 """)
